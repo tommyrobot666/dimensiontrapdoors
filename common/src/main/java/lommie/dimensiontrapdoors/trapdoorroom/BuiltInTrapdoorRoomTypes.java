931ef7e5176f83a1a018191e30131daf7f3ad379
tommyrobot666 <tommyrobot10@gmail.com>
2026-02-12 01:47:28 -0800

    add skyblock, treasure_maze_hidden, watery_coral rooms

--- common/src/main/java/lommie/dimensiontrapdoors/trapdoorroom/BuiltInTrapdoorRoomTypes.java
+++ common/src/main/java/lommie/dimensiontrapdoors/trapdoorroom/BuiltInTrapdoorRoomTypes.java
@@ -26,6 +26,8 @@ public class BuiltInTrapdoorRoomTypes {
         registerType("empty",new BlockPos(8,1,8),1);
         registerType("hallway",new BlockPos(7,1,1),3);
         registerType("water_elevators",new BlockPos(2,6,2),1);
-        registerType("watery_coral",new BlockPos(8,40,8),1);
+        registerType("watery_coral",new BlockPos(8,40,8),1,Identifier.withDefaultNamespace("warm_ocean"));
+        registerType("treasure_maze_hidden",new BlockPos(1,32,1),1);
+        registerType("skyblock",new BlockPos(15*8+7,27,16*8+7),16);
     }
 }
